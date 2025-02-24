ebb7788830dc66626ef17ea1ecf1ea778a23f9b3
SakshiSawant419 <sawant.sak@northeastern.edu>
2025-02-24 18:32:51 -0500

    Update subnets.tf

--- subnets.tf
+++ subnets.tf
@@ -14,7 +14,7 @@ resource "aws_subnet" "public" {
 resource "aws_subnet" "private" {
   count = length(var.private_subnets)
 
-  vpc_id            = aws_vpc.main.id
+  vpc_id            = aws_vpc.main.bad
   cidr_block        = var.private_subnets[count.index]
   availability_zone = var.availability_zones[count.index]
 
