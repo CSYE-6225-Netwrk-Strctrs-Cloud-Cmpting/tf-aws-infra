9bcaeb1ce7bb295cc169e33920c1be1e868425bc
SakshiSawant419 <sawant.sak@northeastern.edu>
2025-02-24 18:30:51 -0500

    Update subnets.tf

--- subnets.tf
+++ subnets.tf
@@ -1,7 +1,7 @@
 resource "aws_subnet" "public" {
   count = length(var.public_subnets)
 
-  vpc_id                  = aws_vpc.main.id
+  vpc_id                  =    aws_vpc.main.id
   cidr_block              = var.public_subnets[count.index]
   availability_zone       = var.availability_zones[count.index]
   map_public_ip_on_launch = true
